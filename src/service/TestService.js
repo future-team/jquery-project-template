ced6e0d115d3b2a87ff64ad7743fee03e69908d3
ainiderenxm <277407493@qq.com>
2016-06-29 20:25:30 +0800

    add registerhelper

--- src/service/TestService.js
+++ src/service/TestService.js
@@ -1,10 +1,23 @@
 import {mask} from 'jq-modal';
 
 import loginTpl from '../template/login.html';
+//如果要调用Handlebars其他方法，如registerHelper，则需要手动引入
+import Handlebars from 'handlebars/runtime';
 
 class TestService{
 
     constructor(){
+
+        Handlebars.registerHelper("debug", function(optionalValue) {
+            console.log("Current Context");
+            console.log("====================");
+            console.log(this);
+            if (optionalValue) {
+                console.log("Value");
+                console.log("====================");
+                console.log(optionalValue);
+            }
+        });
         this.showDialog();
 
         $('#showMask').on('click',()=>{$('#login').mask()});
